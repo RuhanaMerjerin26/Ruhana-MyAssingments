1bd5072e28288290ab36bf1a883028162189eb64
RuhanaMerjerin26 <ruhanamerj@gmail.com>
2026-02-28 19:37:17 +0530

    Add files via upload

---
+++ DataTypesDay01.js
@@ -0,0 +1,10 @@
+var firstName = "Ruhana Merjerin"
+var compznyName = "CGI"
+var mobileNum = 234567893
+var isAutomation = true
+var hasPlaywright
+console.log("Enter the first name : " +firstName)
+console.log("Enter the company name : " +compznyName)
+console.log("Enter the mobile number : " +mobileNum)
+console.log("Is Automation : " +isAutomation)
+console.log(typeof hasPlaywright)
